93dceabce1be32b7de9adfed40090a5f777c5443
Armand Maree <armand.maree@epiuse.com>
2024-11-22 10:10:07 -0500

    Added retry functionality

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import 'dotenv/config'
 
 import FastSpeedtest from 'fast-speedtest-api'
 import { PutMetricDataCommand } from '@aws-sdk/client-cloudwatch'
 import { client } from './client'
 import logger from './logger'
+import { sleep } from './utils'
 
 logger.info('Starting...')
 
@@ -17,36 +19,63 @@ const speedtest = new FastSpeedtest({
   unit: FastSpeedtest.UNITS.Mbps, // default: Bps
 })
 
-logger.info('Speed test running')
+let successfulTest = false
+let numberOfTriesRemaining = Number(process.env.NUMBER_OF_RETRIES ?? 3)
+const delayBetweenTries = Number(process.env.DELAY_BETWEEN_TRIES ?? 5000)
 
-speedtest
-  .getSpeed()
-  .then(async (s: number) => {
-    logger.info(`Speed: ${s} Mbps`)
+const runSpeedTest = async () => {
+  logger.info('Speed test running')
+  let speed
+  try {
+    speed = await speedtest.getSpeed()
+  } catch (error: any) {
+    logger.error('Speed test failed')
+    logger.error(error?.message ?? error)
+    throw error
+  }
 
-    const command = new PutMetricDataCommand({
-      MetricData: [
-        {
-          MetricName: `${process.env.LOCATION}NetworkSpeed`,
-          Unit: 'Megabits/Second',
-          Value: s,
-        },
-      ],
-      Namespace: `NetworkSpeed/${process.env.LOCATION}`,
-    })
+  logger.info(`Speed: ${speed} Mbps`)
 
+  const command = new PutMetricDataCommand({
+    MetricData: [
+      {
+        MetricName: `${process.env.LOCATION}NetworkSpeed`,
+        Unit: 'Megabits/Second',
+        Value: speed,
+      },
+    ],
+    Namespace: `NetworkSpeed/${process.env.LOCATION}`,
+  })
+
+  try {
+    logger.info(`Sending metric to AWS ${process.env.LOCATION}`)
+    await client.send(command)
+    logger.info('Sent metric successfully')
+  } catch (error) {
+    logger.error('Failed to send metric to AWS')
+    logger.error(error)
+    throw error
+  }
+
+  logger.info('Done')
+  successfulTest = true
+}
+
+const main = async () => {
+  while (!successfulTest && numberOfTriesRemaining > 0) {
     try {
-      logger.info(`Sending metric to AWS ${process.env.LOCATION}`)
-      await client.send(command)
-      logger.info('Sent metric successfully')
-    } catch (error) {
-      logger.error('Failed to send metric to AWS')
-      logger.error(error)
+      await runSpeedTest()
+    } catch (error: any) {
+      logger.error('Failed to run speed test')
+      logger.error(error.message ?? error)
     }
 
-    logger.info('Done')
-  })
-  .catch((error: Error) => {
-    logger.error('Speed test failed')
-    logger.error(error.message ?? error)
-  })
+    if (!successfulTest) {
+      logger.error(`Will try again in ${delayBetweenTries} ms`)
+      numberOfTriesRemaining -= 1
+      await sleep(delayBetweenTries)
+    }
+  }
+}
+
+main()
